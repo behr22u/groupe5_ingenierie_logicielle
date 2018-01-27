730b4eceb6b287884f02eb7f7c035dc53c215dd3
Louis <Louis@DESKTOP-P6HA9D9>
2018-01-27 15:37:22 +0100

    Début de la classe Case2

---
+++ projet_2584/src/jeu/Grille.java
@@ -0,0 +1,22 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package jeu;
+
+/**
+ *
+ * @author Louis
+ */
+public class Grille implements Parametres {
+    
+    // A DEFINIR 
+    //static final int HAUT = ?;
+    //static final int BAS = ?;
+    //static final int GAUCHE= ?;
+    //static final int DROITE= ?;
+    
+    static final int TAILLE = 4;
+    static final int OBJECTIF = 2584;
+}
