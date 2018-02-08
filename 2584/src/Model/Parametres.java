4dc0251514b28d65dc14d448850dde49451bd395
crumbleauxfruits <lea.tham@live.fr>
2018-02-08 17:15:40 +0100

    recup des vieux trucs

---
+++ 2584/src/Model/Parametres.java
@@ -0,0 +1,20 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+package Model;
+
+/**
+ *
+ * @author leath
+ */
+public interface Parametres {
+    static final int HAUT = 1;
+    static final int DROITE = 2;
+    static final int BAS = -1;
+    static final int GAUCHE = -2;
+    static final int TAILLE = 4;
+    static final int OBJECTIF = 25584;
+}
