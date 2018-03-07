49ecd6230324e0addb5bbb7ab368f70ef3f56071
TheoDriutti <theo.driutti57@gmail.com>
2018-03-07 18:16:52 +0100

    debut undo
    début création classe joueur + fonction undo dans joueur

---
+++ 2584/src/Model/Joueur.java
@@ -0,0 +1,24 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package Model;
+
+/**
+ *
+ * @author theod
+ */
+public class Joueur {
+    private Grille grilleActu;
+    private Grille grilleSauv;
+    private int nbUndo;
+    
+    public void undo(){
+        if (nbUndo>0){
+            this.grilleActu=this.grilleSauv;
+            nbUndo-=1;
+        }
+    }
+    
+}
