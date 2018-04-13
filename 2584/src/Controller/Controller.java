df69df12ba46a556dfff306e92be9217b8fb14bb
TheoDriutti <theo.driutti57@gmail.com>
2018-04-13 22:29:16 +0200

    début javadoc

--- 2584/src/Controller/Controller.java
+++ 2584/src/Controller/Controller.java
@@ -1,9 +1,8 @@
- /*
+/*
  * To change this license header, choose License Headers in Project Properties.
  * To change this template file, choose Tools | Templates
  * and open the template in the editor.
  */
-
 package Controller;
 
 import Model.Case;
@@ -47,12 +46,11 @@ import javafx.scene.layout.Pane;
  *
  * @author leath
  */
+public class Controller implements Initializable, Parametres {
 
-
-public class Controller implements Initializable, Parametres{
     public static ArrayList<Integer> termesFibonacci = SuitesMathematiques.fibonacci(OBJECTIF);
     public static Partie partie = new Partie();
-    
+
     /*
      * Variables globales correspondant à des objets définis dans la vue (fichier .fxml)
      * Ces variables sont ajoutées à la main et portent le même nom que les fx:id dans Scene Builder
@@ -71,65 +69,70 @@ public class Controller implements Initializable, Parametres{
     private Label nbUndo2;
     @FXML
     private static Label winner;
-    
+
     @FXML
     private GridPane gridpane;
     @FXML
     private Pane fond; // panneau recouvrant toute la fenêtre
     @FXML
     private Pane fond_case; // panneau utilisé pour la mise à jour des cases, suppression/reapparition
-    
+
     // boutons utilisé dans le cas du Undo
     @FXML
     private Button undoj1;
     @FXML
     private Button undoj2;
-    
+
     // menu déroulant
     @FXML
     private ComboBox choix;
-    
+
     // bouton qui démarre le jeu
     @FXML
     private Button start;
-    
-    
-    
-    public static void lancementJeuGraphique(String val){
-        
-        switch(val){
-            case "Joueur vs Joueur" : Controller.partie.setVs(VSJOUEUR);
-            break;
-            
-            case "Joueur vs Random" : Controller.partie.setVs(VSRANDOM);
-            break;
-            
-            case "IA vs Joueur" : Controller.partie.setVs(VSIA);
-            break;
-            
-            case "IA vs Random" : Controller.partie.setVs(VSIARANDOM);
-            break;
+
+    /**
+     * Paramètre le type de partie qui va être jouée.
+     *
+     * @param val La chaîne qui décrit le type de partie.
+     */
+    public static void lancementJeuGraphique(String val) {
+
+        switch (val) {
+            case "Joueur vs Joueur":
+                Controller.partie.setVs(VSJOUEUR);
+                break;
+
+            case "Joueur vs Random":
+                Controller.partie.setVs(VSRANDOM);
+                break;
+
+            case "IA vs Joueur":
+                Controller.partie.setVs(VSIA);
+                break;
+
+            case "IA vs Random":
+                Controller.partie.setVs(VSIARANDOM);
+                break;
         }
-        
+
         Grille g1 = new Grille();
         boolean b = g1.nouvelleCase();
         b = g1.nouvelleCase(1);
         Joueur j1 = new Joueur(g1);
         Grille g2 = (Grille) g1.clone();
-        
-        if(Controller.partie.getVs() == VSRANDOM){
+
+        if (Controller.partie.getVs() == VSRANDOM) {
             RandomPlayer rp = new RandomPlayer(g2);
             Controller.partie.setJoueurs(j1, rp);
-        }else{
+        } else {
             Joueur j2 = new Joueur(g2);
             Controller.partie.setJoueurs(j1, j2);
         }
-        
+
         System.out.println(g1);
         System.out.println(g2);
-        
- 
-        
+
         /*
         for( int i = 0 ; i < NOMBREDEJOUEURS ; i++ ){
             System.out.println("Grille du joueur" + i);
@@ -140,22 +143,20 @@ public class Controller implements Initializable, Parametres{
             System.out.println(g);
             partie.setG(i,g);
         }
-        */
+         */
     }
-  
-   
+
     @Override
-    public void initialize(URL url, ResourceBundle rb) {  
+    public void initialize(URL url, ResourceBundle rb) {
         // TODO
         System.out.println("le contrôleur initialise la vue");
-        
+
         //pré-choix d'aversaire via menu déroulant
         //this.afficheTableau();
-        
         // si on clique sur le bouton undoj1
         undoj1.setOnAction(new EventHandler<ActionEvent>() {
             @Override
-            public void handle(ActionEvent event){
+            public void handle(ActionEvent event) {
                 Controller.partie.getJ(0).undo();
                 nbUndo1.setText(Integer.toString(partie.getJ(0).getNbUndo()));
                 //undoj1.setDisable(true);
@@ -165,11 +166,11 @@ public class Controller implements Initializable, Parametres{
                 afficheTableau();
             }
         });
-        
+
         // si on clique sur le bouton undoj2
         undoj2.setOnAction(new EventHandler<ActionEvent>() {
             @Override
-            public void handle(ActionEvent event){
+            public void handle(ActionEvent event) {
                 Controller.partie.getJ(1).undo();
                 nbUndo2.setText(Integer.toString(partie.getJ(1).getNbUndo()));
                 // si on utilise setDisable(true), il n'est plus possible d'utiliser la methode keyPressed.. aucune idée pourquoi..
@@ -179,44 +180,43 @@ public class Controller implements Initializable, Parametres{
                 // on affiche les labels à leur nouvel emplacement ainsi que les nouveaux labels (les cases)
                 afficheTableau();
             }
-       });
-        
-        
-        start.setOnAction(new EventHandler<ActionEvent>(){
-           @Override
-           public void handle(ActionEvent event){               
-               String val_choix = (String) choix.getValue();
-               fond_case.getChildren().clear();
-               lancementJeuGraphique(val_choix);
-               // on vide le panneau contenant les différents label représenant les cases
-               viderGrid(fond_case);
-               // on affiche les labels à leur nouvel emplacement ainsi que les nouveaux labels (les cases)
-               afficheTableau();
-           }
-        });  
-        
+        });
+
+        start.setOnAction(new EventHandler<ActionEvent>() {
+            @Override
+            public void handle(ActionEvent event) {
+                String val_choix = (String) choix.getValue();
+                fond_case.getChildren().clear();
+                lancementJeuGraphique(val_choix);
+                // on vide le panneau contenant les différents label représenant les cases
+                viderGrid(fond_case);
+                // on affiche les labels à leur nouvel emplacement ainsi que les nouveaux labels (les cases)
+                afficheTableau();
+            }
+        });
+
     }
-    
+
     /**
-     * Méthode qui permet d'afficher les visuels des tableaux
-     * On y appelle le css
-     * On affiche les différents labels qui représentent les cases avec leur valeurs
+     * Méthode qui permet d'afficher les visuels des tableaux. On y appelle le
+     * css. On affiche les différents labels qui représentent les cases avec
+     * leur valeurs.
      */
-    public void afficheTableau(){
-        for( int i = 0 ; i < NOMBREDEJOUEURS ; i++ ){
-           // System.out.println("ici 1 : " + i);
-            HashSet<Case> g = partie.getHashGrille(i); 
-            
+    public void afficheTableau() {
+        for (int i = 0; i < NOMBREDEJOUEURS; i++) {
+            // System.out.println("ici 1 : " + i);
+            HashSet<Case> g = partie.getHashGrille(i);
+
             gridpane = new GridPane();
             //System.out.println("ici 2 : " + i);
-            for(Case c : g){
-              //  System.out.println("ici 3 : " + i);
+            for (Case c : g) {
+                //  System.out.println("ici 3 : " + i);
                 Pane p = new Pane();
                 Label l = new Label(Integer.toString(c.getValeur()));
                 gridpane.add(l, c.getX(), c.getY());
-               // System.out.println("ici 4 : " + i);
+                // System.out.println("ici 4 : " + i);
                 // utilisation de styles pour la grille et la tuile (voir styles.css)
-                p.getStyleClass().add("pane"); 
+                p.getStyleClass().add("pane");
                 //System.out.println("ici 5 : " + i);
                 l.getStyleClass().add("tuile");
                 //System.out.println("ici 6 : " + i);
@@ -228,36 +228,39 @@ public class Controller implements Initializable, Parametres{
                 //System.out.println("ici 9 : " + i);
                 p.getChildren().add(l);
                 // on place la tuile en précisant les coordonnées (x,y) du coin supérieur gauche 
-                
-                switch(i){
-                    
-                    case 0 :  p.setLayoutX(24 + c.getX()*100);
-                              p.setLayoutY(191 + c.getY()*100);
-                              break;
-                              
-                    case 1 :  p.setLayoutX(618 + c.getX()*100);
-                              p.setLayoutY(191 + c.getY()*100);
-                              break;
+
+                switch (i) {
+
+                    case 0:
+                        p.setLayoutX(24 + c.getX() * 100);
+                        p.setLayoutY(191 + c.getY() * 100);
+                        break;
+
+                    case 1:
+                        p.setLayoutX(618 + c.getX() * 100);
+                        p.setLayoutY(191 + c.getY() * 100);
+                        break;
                 }
-               
+
                 p.setVisible(true);
                 l.setVisible(true);
-                
+
             }
         }
     }
-    
-    
+
     /**
      * methode permettant de vider completement le panneau rentrée en parametres
-     * On l'utilise dans la methode keyPressed, juste avant l'appel d'afficherTableau qui permet d'afficher les nouveaux emplacements des cases
-     * @param pane 
+     * On l'utilise dans la methode keyPressed, juste avant l'appel
+     * d'afficherTableau qui permet d'afficher les nouveaux emplacements des
+     * cases
+     *
+     * @param pane Le panneau que l'on vide.
      */
-    public void viderGrid(Pane pane){
-            pane.getChildren().clear();
+    public void viderGrid(Pane pane) {
+        pane.getChildren().clear();
     }
 
-       
     @FXML
     private void handleButtonAction(MouseEvent event) {
         System.out.println("Clic de souris sur le bouton menu");
@@ -265,21 +268,21 @@ public class Controller implements Initializable, Parametres{
 
     @FXML
     public void keyPressed(KeyEvent ke) {
-        
-        if(!finDePartie()){
+
+        if (!finDePartie()) {
             System.out.println("touche appuyée");
             System.out.println("type vs = " + Controller.partie.getVs());
             String touche = ke.getText();
-            if (Controller.partie.getVs() != VSIARANDOM){
+            if (Controller.partie.getVs() != VSIARANDOM) {
                 /// on teste les directions du joueur 1
                 int direction = convertDirectionJ1(touche);
                 boolean b1 = false;
-                if (direction != 0){
+                if (direction != 0) {
                     b1 = partie.getJ(0).jouer(direction);
                     if (b1) {
                         boolean b = partie.getG(0).nouvelleCase();
                         //if (!b) partie.getG(0).gameOver();
-                        if (Controller.partie.getJ(0).undoPossible()){
+                        if (Controller.partie.getJ(0).undoPossible()) {
                             undoj1.setDisable(false);
                         }
                     }
@@ -292,19 +295,19 @@ public class Controller implements Initializable, Parametres{
                     move1.setText(Integer.toString(partie.getJ(0).getDeplacement()));
                     score1.setText(Integer.toString(partie.getJ(0).getScore()));
                 }
-                if (Controller.partie.getVs() == VSRANDOM && b1){
+                if (Controller.partie.getVs() == VSRANDOM && b1) {
                     System.out.println(Controller.partie.getJ(1).getClass());
                     Controller.partie.getJ(1).jouer();
                 }
-                if (Controller.partie.getVs() == VSJOUEUR){
+                if (Controller.partie.getVs() == VSJOUEUR) {
                     direction = convertDirectionJ2(touche);
                     boolean b2 = false;
-                    if (direction != 0){
+                    if (direction != 0) {
                         b2 = partie.getJ(1).jouer(direction);
                         if (b2) {
                             boolean b = partie.getG(1).nouvelleCase();
                             //if (!b) partie.getG(1).gameOver();
-                            if (Controller.partie.getJ(1).undoPossible()){
+                            if (Controller.partie.getJ(1).undoPossible()) {
                                 undoj2.setDisable(false);
                             }
                         }
@@ -314,11 +317,9 @@ public class Controller implements Initializable, Parametres{
                         partie.getJ(1).addDeplacement();
                         //on modifie le label move2
                         move2.setText(Integer.toString(partie.getJ(1).getDeplacement()));
-                        score2.setText(Integer.toString(partie.getJ(1).getScore()));                    
+                        score2.setText(Integer.toString(partie.getJ(1).getScore()));
                     }
                 }
-
-
 
                 // on vide le panneau contenant les différents label représenant les cases
                 viderGrid(fond_case);
@@ -327,17 +328,15 @@ public class Controller implements Initializable, Parametres{
             }
         }
     }
-    
-    
-    
+
     /**
      * Méthode de lancement du Jeu, sans interface graphique, dans la console
      */
-     static public void lancementJeu(){
+    static public void lancementJeu() {
         Grille grilles[] = new Grille[NOMBREDEJOUEURS];
-        for( int i = 0 ; i < NOMBREDEJOUEURS ; i++ ){
+        for (int i = 0; i < NOMBREDEJOUEURS; i++) {
             System.out.println("Grille du joueur" + i);
-            
+
             grilles[i] = new Grille();
             boolean b = grilles[i].nouvelleCase(1);
             b = grilles[i].nouvelleCase(1);
@@ -346,7 +345,7 @@ public class Controller implements Initializable, Parametres{
         Scanner sc = new Scanner(System.in);
 
         while (!finDePartie(grilles)) {
-            for (int i=0 ; i<NOMBREDEJOUEURS ; i++ ){
+            for (int i = 0; i < NOMBREDEJOUEURS; i++) {
                 System.out.println("C'est a vous joueur " + i + " !");
                 System.out.println("Déplacer vers la Droite (d), Gauche (g), Haut (h), ou Bas (b) ?");
                 String s = sc.nextLine();
@@ -370,76 +369,82 @@ public class Controller implements Initializable, Parametres{
                     boolean b2 = grilles[i].lanceurDeplacerCases(direction);
                     if (b2) {
                         boolean b = grilles[i].nouvelleCase();
-                        if (!b) grilles[i].gameOver();
+                        if (!b) {
+                            grilles[i].gameOver();
+                        }
                     }
                     System.out.println(grilles[i]);
-                    if (grilles[i].getValeurMax()>=OBJECTIF) grilles[i].victory();
+                    if (grilles[i].getValeurMax() >= OBJECTIF) {
+                        grilles[i].victory();
+                    }
                 }
             }
         }
-        for(Grille g : grilles){
+        for (Grille g : grilles) {
             g.gameOver();
         }
-            
+
     }
-     
-     /**
-      * Méthode qui permet de déterminer si la partie est finie, version console
-      * @param grilles
-      * @return un boolean : true --> la partie est finie ; false --> la partie n'est pas finie
-      */
-     static private boolean finDePartie(Grille grilles[]){
+
+    /**
+     * Méthode qui permet de déterminer si la partie est finie, version console
+     *
+     * @param grilles
+     * @return un boolean : true --> la partie est finie ; false --> la partie
+     * n'est pas finie
+     */
+    static private boolean finDePartie(Grille grilles[]) {
         boolean fini = false;
-        for(Grille g : grilles){
-            if (g.partieFinie()){
+        for (Grille g : grilles) {
+            if (g.partieFinie()) {
                 fini = true;
-                
-                if(g.getValeurMax() == OBJECTIF){
+
+                if (g.getValeurMax() == OBJECTIF) {
                     g.victory();
                 }
             }
         }
-        if (fini){
+        if (fini) {
             EchangeBDD.insertPartie(Controller.partie);
         }
         return fini;
     }
-     
-     /**
-      * méthode qui permet de déterminer si la partie est finie et d'afficher le vainqueur dans un label
-      * @return un boolean afin de savoir si la partie est finie ou pas
-      */
-     static private boolean finDePartie(){
-         
+
+    /**
+     * méthode qui permet de déterminer si la partie est finie et d'afficher le
+     * vainqueur dans un label
+     *
+     * @return un boolean afin de savoir si la partie est finie ou pas
+     */
+    static private boolean finDePartie() {
+
         boolean fini = false;
-        
+
         //on parcours le nombre de joueurs
-        for( int i = 0 ; i < NOMBREDEJOUEURS ; i++ ){
+        for (int i = 0; i < NOMBREDEJOUEURS; i++) {
             //on attribue à un objet grille la grille qui correspond au joueur i
             Grille g = Controller.partie.getG(i);
             // si la grille contient plus de 16 cases
-            if (g.partieFinie()){
+            if (g.partieFinie()) {
                 fini = true;
-                if(i == 0){
+                if (i == 0) {
                     winner.setText("Joueur 2");
-                } else{
+                } else {
                     winner.setText("Joueur 1");
                 }
                 //g.gameOver();
 
-                
-                
-            } else{
-               if(g.getValeurMax() == OBJECTIF){
+            } else {
+                if (g.getValeurMax() == OBJECTIF) {
                     winner.setText(String.valueOf(partie.getJ(i)));
-                   // g.victory();                
-                } 
-            }                
+                    // g.victory();                
+                }
+            }
         }
-        if (fini){
+        if (fini) {
             EchangeBDD.insertPartie(Controller.partie);
         }
         return fini;
     }
-  
+
 }
