3f93f4532313010445a3503c651ba04d19febdf6
behr22u <louis2.behr@laposte.net>
2018-04-13 16:15:26 +0200

    requette recupération des parties

--- 2584/src/Model/EchangeBDD.java
+++ 2584/src/Model/EchangeBDD.java
@@ -24,64 +24,60 @@ public class EchangeBDD {
     private static String query;
     private static ResultSet rs;
     
-    public static void connect(){
+    public static void recupParties(){
         try {
-                Class.forName("com.mysql.jdbc.Driver").newInstance();
-                con = DriverManager.getConnection(connectUrl, username, password);
-                System.out.println("Database connection established.");
+            Class.forName("com.mysql.jdbc.Driver").newInstance();
+            con = DriverManager.getConnection(connectUrl, username, password);
+            System.out.println("Database connection established.");
 
-                Statement stmt = con.createStatement();
+            Statement stmt = con.createStatement();
 
-                //recupération des 10 dernières parties
-                System.out.println("recup id et tuile max des 10 dernières parties");
-                query = "SELECT id, valeurMax FROM `partie` ORDER BY ID desc  LIMIT 10 ";
-                ArrayList<String> id = new ArrayList<>();
-                ArrayList<String> valeurMax = new ArrayList<>();
-                rs = stmt.executeQuery(query);
-                while (rs.next()) {
-                    id.add(rs.getString("id"));
-                    valeurMax.add(rs.getString("valeurMax"));
-                }
-                
-               
-                /*
-                for (int i=0; i<id.size();i++){
-                    System.out.println(titres.get(i));
-                }
-                */
+            //recupération des 10 dernières parties
+            System.out.println("recup id et tuile max des 10 dernières parties");
+            query = "SELECT id, valeurMax FROM `partie` ORDER BY ID desc  LIMIT 10 ";
+            ArrayList<String> id = new ArrayList<>();
+            ArrayList<String> valeurMax = new ArrayList<>();
+            rs = stmt.executeQuery(query);
+            while (rs.next()) {
+                id.add(rs.getString("id"));
+                valeurMax.add(rs.getString("valeurMax"));
+            }
 
-                //requete recupération des scores et nombre de déplacements 
-                System.out.println("recup score et deplacement");
-                query = "SELECT score, déplacement FROM Joueur WHERE id_partie=";
+            System.out.println("Historique des parties : ");
+            for (int i=0; i<id.size();i++){
+                //requete recupération des scores et nombre de déplacements
+                query = "SELECT score, deplacement FROM Joueur WHERE id_partie=" + id.get(i);
                 ArrayList<String> score = new ArrayList<>();
                 ArrayList<String> deplacement = new ArrayList<>();
                 rs = stmt.executeQuery(query);
                 while (rs.next()) {
-                    noms.add(rs.getString("nom"));
-                    prenoms.add(rs.getString("prenom"));
+                    score.add(rs.getString("score"));
+                    deplacement.add(rs.getString("deplacement"));
                 }
-                for (int i=0; i<noms.size();i++){
-                    System.out.println(noms.get(i)+" "+prenoms.get(i));
+                System.out.println("Valeur max de la partie : " + valeurMax.get(i));
+                for (int j=0 ; j<score.size() ; j++){
+                    System.out.println("Joueur " + (j+1) + " : score : " + score.get(j) + " , Nombre de déplacements : " + deplacement.get(j));
                 }
-
-                
-            } catch (ClassNotFoundException cnfe) {
-                System.out.println("Cannot load db driver: com.mysql.jdbc.Driver");
-                cnfe.printStackTrace();
-            } catch (SQLException se) {
-                System.out.println("Avez-vous pensé à démarrer EasyPHP/Wamp ?");
-                se.printStackTrace();
-            } catch (Exception e) {
-                System.out.println("Erreur inattendue");
-                e.printStackTrace();
-            } finally {
-                // à la fin, on ferme la connection avec la BdD
-                if (con != null) {
-                    try {
-                        con.close();
-                        System.out.println("Database connection terminated.");
-                    } catch (Exception e) { /* ignore close errors */ }
-                }
+                System.out.println("------------------");
             }
+
+        } catch (ClassNotFoundException cnfe) {
+            System.out.println("Cannot load db driver: com.mysql.jdbc.Driver");
+            cnfe.printStackTrace();
+        } catch (SQLException se) {
+            System.out.println("Avez-vous pensé à démarrer EasyPHP/Wamp ?");
+            se.printStackTrace();
+        } catch (Exception e) {
+            System.out.println("Erreur inattendue");
+            e.printStackTrace();
+        } finally {
+            // à la fin, on ferme la connection avec la BdD
+            if (con != null) {
+                try {
+                    con.close();
+                    System.out.println("Database connection terminated.");
+                } catch (Exception e) { /* ignore close errors */ }
+            }
+        }
     }
 }
